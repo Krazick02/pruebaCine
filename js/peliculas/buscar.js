9c81524b31a08832cfa20e405aa765eb722ab2c1
Angel De Jesus Aviles Cota <63378450+Krazick02@users.noreply.github.com>
2022-09-22 03:20:26 -0600

    Add files via upload

--- js/peliculas/buscar.js
+++ js/peliculas/buscar.js
@@ -6,7 +6,7 @@ const myApp = createApp({
     data() {
         return {
             resultado: '',
-            busqueda:'bad boys',
+            busqueda: 'bad boys',
             imgRoute: 'https://image.tmdb.org/t/p/w500'
         }
     },
@@ -15,25 +15,34 @@ const myApp = createApp({
             localStorage.setItem('id', p.id);
             window.location.href = "../pelicula.html";
         },
-        tv(){
-            localStorage.setItem('media_type','tv')
+        tv() {
+            localStorage.setItem('media_type', 'tv')
+            this.getPeliculas()
         },
-        movie(){
-            localStorage.setItem('media_type','movie')
+        movie() {
+            localStorage.setItem('media_type', 'movie')
+            this.getPeliculas()
         },
-        buscar(e){
-            e.preventDefault();
+        palabra() {
+            this.getPalabra()
+        },
+        categoria() {
             this.getPeliculas()
         },
         getPeliculas() {
-            var media=localStorage.getItem('media_type')
-            fetch('https://api.themoviedb.org/3/search/'+media+'?api_key=038eb1b67a577ce9afaf326a641856f8&/&query='+this.busqueda)
+            var media = localStorage.getItem('media_type')
+            fetch('https://api.themoviedb.org/3/search/' + media + '?api_key=038eb1b67a577ce9afaf326a641856f8&/&query=' + this.busqueda)
+                .then((response) => response.json())
+                .then((res) => (this.resultado = res.results))
+                .catch((error) => console.log('error', error));
+        },
+        coincidencias(n) {
+            var media = localStorage.getItem('media_type')
+            fetch('https://api.themoviedb.org/3/discover/' + media + '?api_key=038eb1b67a577ce9afaf326a641856f8&language=en-US&with_genres=' + n + '&sort_by=popularity.desc')
                 .then((response) => response.json())
                 .then((res) => (this.resultado = res.results))
                 .catch((error) => console.log('error', error));
         }
     },
-    mounted() {
-    }
+    mounted() {}
 }).mount('#buscando')
-
